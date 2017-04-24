be13a08bf4af4c9c987c90e99d6d30861b630bac
Danny Dulong <dannydulong@gmail.com>
2017-04-24 17:33:46 -0400

    Fichier hello world

--- 01-hello_world.sh
+++ 01-hello_world.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
 
+
+echo "salut"
+#exit
+echo "toi"
+echo -n "Vive"
+echo " Linux"
+
+echo -e "1\n2\n3"
+
+#-n reste sur la meme ligne
+#-e prend comme en c \n \t
+
+read -p "Quel âge avez-vous?" age
+echo $age "ans! Vous paraissez  jeune!"
+#$ pour avoir la valeur de la variable, lors de la saisie pas de $
+
+
