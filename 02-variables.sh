e757409364a99da3c472cd82271dcb67152722f2
Danny Dulong <dannydulong@gmail.com>
2017-04-24 17:41:11 -0400

    Variable

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+prenom=John
+#ne pas mettre d'espace
+echo $prenom
+
+declare -i age=35 #int
+declare -r age=33 #devient lecture seule constante
+
+cours="Cours de linux" #pour mettre des espaces mettre entre  guillemets
+
+echo Le programme $0 a pris le paramètre $1
