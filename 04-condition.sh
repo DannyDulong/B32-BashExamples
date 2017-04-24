6c8f7055cf3f21902d830279f03fca90fddb8880
Danny Dulong <dannydulong@gmail.com>
2017-04-24 17:54:39 -0400

    Condition case et if

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then
+echo "You failed"
+elif test $note -eq 60
+then
+echo "ouf!c'est juste"
+else
+echo "alright"
+fi
 
+read -p "Entrez une lettre : " lettre
 
+case $lettre in
+	[[:lower:]])
+		echo "lettre en minuscule"
+		;;
+	*)
+		echo "Majuscule"
+		;;
+esac
 
-
-
+if test $lettre != "a"
+then
+echo "la lettre n'est pas a"
+fi
 
