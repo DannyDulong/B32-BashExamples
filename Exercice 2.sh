d834dec6f0f5c21efdce8d42c69b51cbbfab41a7
Danny Dulong <dannydulong@gmail.com>
2017-04-26 16:42:26 -0400

    Animation bombe

--- Exercice 2.sh
+++ Exercice 2.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+affMenu(){
+echo "Bievenue"
+echo "Choix a"
+echo "Choix b"
+echo "Choix c"
+echo "Appuyer sur q pour quitter"
+}
+option=o
+while test $option != q
+do
+clear
+affMenu
+read -p "Entrez un choix: " option
+if test $option = "a"
+then
+./Exercice\ 1.sh
+sleep
+fi
+done
--- Exercice 3.sh
+++ Exercice 3.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+faireEspace()
+{
+clear
+	for((i=0;i<$1;i++))
+	do
+	echo  -n " "
+	done
+}
+for ((i=0;i<30;i++))
+do
+faireEspace $i
+echo "@"
+sleep 0.05
+done
+faireEspace 30
+echo "Boum"	
+sleep 5;clear
